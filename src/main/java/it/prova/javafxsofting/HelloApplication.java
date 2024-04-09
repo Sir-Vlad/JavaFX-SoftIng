e37a29b2207718d2296579af8d936ea983eb32b9
/JACO <117900254+djacoo@users.noreply.github.com>
2024-04-09 08:48:50 +0200

    deciso traccia!

--- src/main/java/it/prova/javafxsofting/HelloApplication.java
+++ src/main/java/it/prova/javafxsofting/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(
                 HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Abbiamo scelto il configuratore di auto ciao!");
+        stage.setTitle("Abbiamo scelto il configuratore di auto ciao! Porcodio!");
         stage.setScene(scene);
         stage.show();
     }
