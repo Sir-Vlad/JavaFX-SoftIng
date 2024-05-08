5b5249b1c880cbba9e49b982d1855f2d5e0dada9
Sir-Vlad <mattiafrigiola01@gmail.com>
2024-05-08 15:30:06 +0200

    Fix db e setting page amministrazione

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = "Backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": BASE_DIR / "db.postgresql",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite",
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "it"
 
 TIME_ZONE = "UTC"
 
--- Backend/Backend_IngSoft/models.py
+++ Backend/Backend_IngSoft/models.py
@@ -1,12 +1,12 @@
+from datetime import datetime
+
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import CASCADE
 
 
 class Utente(models.Model):
-    email = models.CharField(
-        max_length=100, unique=True, null=False, blank=False
-    )
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False)
     password = models.CharField(max_length=20, null=False, blank=False)
     nome = models.CharField(max_length=20, null=False, blank=False)
     cognome = models.CharField(max_length=20, null=False, blank=False)
@@ -26,9 +26,8 @@ class ModelloAuto(models.Model):
         BMW = "BMW"
 
     nome = models.CharField(max_length=20, unique=True, null=False, blank=False)
-    marca = models.CharField(
-        max_length=20, null=False, blank=False, choices=MarcaAuto
-    )  # lista di valori noti
+    marca = models.CharField(max_length=20, null=False, blank=False,
+        choices=MarcaAuto)  # lista di valori noti
     # descrizione = models.TextField()
     prezzo_base = models.IntegerField(null=False, blank=False)
     # dati auto
@@ -67,46 +66,38 @@ class Sede(models.Model):
 
 
 class Impiegato(models.Model):
-    email = models.CharField(
-        max_length=100, unique=True, null=False, blank=False
-    )
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False)
     password = models.CharField(max_length=20, null=False, blank=False)
     nome = models.CharField(max_length=20, null=False, blank=False)
     cognome = models.CharField(max_length=20, null=False, blank=False)
 
 
 class Configurazione(models.Model):
-    modello = models.ForeignKey(
-        ModelloAuto, on_delete=CASCADE, null=False, blank=False
-    )
+    modello = models.ForeignKey(ModelloAuto, on_delete=CASCADE, null=False, blank=False)
     # required
-    colore = models.ForeignKey(
-        Optional, on_delete=CASCADE, null=False, blank=False
-    )
-    cambio = models.ForeignKey(
-        Optional, on_delete=CASCADE, null=False, blank=False
-    )
-    motorizzazione = models.ForeignKey(
-        Optional, on_delete=CASCADE, null=False, blank=False
-    )
+    colore = models.ForeignKey(Optional, on_delete=CASCADE, null=False, blank=False,
+        related_name='colore')
+    cambio = models.ForeignKey(Optional, on_delete=CASCADE, null=False, blank=False,
+        related_name='cambio')
+    motorizzazione = models.ForeignKey(Optional, on_delete=CASCADE, null=False,
+        blank=False, related_name='motorizzazione')
     # optional
-    dim_cerchi = models.ForeignKey(Optional, on_delete=CASCADE)
-    stereo = models.ForeignKey(Optional, on_delete=CASCADE)
-    fanali = models.ForeignKey(Optional, on_delete=CASCADE)
-    finestrini = models.ForeignKey(Optional, on_delete=CASCADE)
-    scarico = models.ForeignKey(Optional, on_delete=CASCADE)
-    sospensioni = models.ForeignKey(Optional, on_delete=CASCADE)
-    sedili = models.ForeignKey(Optional, on_delete=CASCADE)
-    infotainment = models.ForeignKey(Optional, on_delete=CASCADE)
+    dim_cerchi = models.ForeignKey(Optional, on_delete=CASCADE, related_name='dim_cer')
+    stereo = models.ForeignKey(Optional, on_delete=CASCADE, related_name='stereo')
+    fanali = models.ForeignKey(Optional, on_delete=CASCADE, related_name='fanali')
+    finestrini = models.ForeignKey(Optional, on_delete=CASCADE, related_name='finestrini')
+    scarico = models.ForeignKey(Optional, on_delete=CASCADE, related_name='scarico')
+    sospensioni = models.ForeignKey(Optional, on_delete=CASCADE,
+                                    related_name='sospensioni')
+    sedili = models.ForeignKey(Optional, on_delete=CASCADE, related_name='sedili')
+    infotainment = models.ForeignKey(Optional, on_delete=CASCADE,
+                                     related_name='infotainment')
 
 
 class Preventivo(models.Model):
-    utente = models.ForeignKey(
-        Utente, on_delete=CASCADE, null=False, blank=False
-    )
-    configurazione = models.ForeignKey(
-        Configurazione, on_delete=CASCADE, null=False, blank=False
-    )
+    utente = models.ForeignKey(Utente, on_delete=CASCADE, null=False, blank=False)
+    configurazione = models.ForeignKey(Configurazione, on_delete=CASCADE, null=False,
+        blank=False)
     data_emissione = models.DateField(null=False, blank=False)
     sede = models.ForeignKey(Sede, on_delete=CASCADE, null=False, blank=False)
     # detrazione = models.ManyToManyField(Detrazione, on_delete=CASCADE)
@@ -114,17 +105,19 @@ class Preventivo(models.Model):
 
 
 class Acquisto(models.Model):
-    numero_fattura = models.CharField(
-        max_length=10, unique=True, null=False, blank=False
-    )
-    utente = models.ForeignKey(
-        Utente, on_delete=CASCADE, null=False, blank=False
-    )
-    preventivo = models.ForeignKey(
-        Preventivo, on_delete=CASCADE, null=False, blank=False
-    )
+    numero_fattura = models.CharField(max_length=10, unique=True, null=False, blank=False)
+    utente = models.ForeignKey(Utente, on_delete=CASCADE, null=False, blank=False)
+    preventivo = models.ForeignKey(Preventivo, on_delete=CASCADE, null=False, blank=False)
     acconto = models.IntegerField(null=False, blank=False)
     data_ritiro = models.DateField(null=False, blank=False)
+
+
+def year_choice():
+    return [(r, r) for r in range(2000, current_year() + 1)]
+
+
+def current_year():
+    return datetime.now().year
 
 
 class Periodo(models.Model):
@@ -143,33 +136,24 @@ class Periodo(models.Model):
         dicembre = "dicembre"
 
     mese = models.CharField(max_length=9, choices=Mesi, null=False, blank=False)
-    anno = models.IntegerField(
-        null=False, blank=False, validators=[MaxValueValidator(9999)]
-    )
+    anno = models.IntegerField(choices=year_choice(), default=current_year(),
+                               null=False, blank=False,
+                               validators=[MaxValueValidator(9999)])
 
     class Meta:
         unique_together = ("mese", "anno")
 
 
 class Sconto(models.Model):
-    periodo = models.ForeignKey(
-        Periodo, on_delete=CASCADE, null=False, blank=False
-    )
-    modello = models.ForeignKey(
-        ModelloAuto, on_delete=CASCADE, null=False, blank=False
-    )
+    periodo = models.ForeignKey(Periodo, on_delete=CASCADE, null=False, blank=False)
+    modello = models.ForeignKey(ModelloAuto, on_delete=CASCADE, null=False, blank=False)
     percentuale_sconto = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(100)]
-    )  # valori da 1 a 100
+        validators=[MinValueValidator(1), MaxValueValidator(100)])  # valori da 1 a 100
 
 
 class Possiede(models.Model):
-    modello = models.ForeignKey(
-        ModelloAuto, on_delete=CASCADE, null=False, blank=False
-    )
-    optional = models.ForeignKey(
-        Optional, on_delete=CASCADE, null=False, blank=False
-    )
+    modello = models.ForeignKey(ModelloAuto, on_delete=CASCADE, null=False, blank=False)
+    optional = models.ForeignKey(Optional, on_delete=CASCADE, null=False, blank=False)
 
     class Meta:
         unique_together = ("modello", "optional")
@@ -184,9 +168,7 @@ class Ritiro(models.Model):
 
 
 class AutoUsata(models.Model):
-    utente = models.ForeignKey(
-        Utente, on_delete=CASCADE, null=False, blank=False
-    )
+    utente = models.ForeignKey(Utente, on_delete=CASCADE, null=False, blank=False)
     modello = models.CharField(max_length=20, null=False, blank=False)
     marca = models.CharField(max_length=20, null=False, blank=False)
     prezzo = models.IntegerField(default=0, null=False, blank=False)
