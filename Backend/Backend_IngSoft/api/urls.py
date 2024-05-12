50008bbc50361e65b9a25da9efcd69c99808ac2f
Sir-Vlad <mattiafrigiola01@gmail.com>
2024-05-12 19:36:00 +0200

    Fix utente api

--- Backend/Backend_IngSoft/api/urls.py
+++ Backend/Backend_IngSoft/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from Backend_IngSoft.api.views import UtenteDetailAPIView, UtenteListCreateAPIView
 
 urlpatterns = [
-    path("utente/", UtenteListCreateAPIView.as_view(), name="utente-list"),
-    path("utente/<int:pk>/", UtenteDetailAPIView.as_view(), name="utente-detail"),
+    path("utenti/", UtenteListCreateAPIView.as_view(), name="utente-list"),
+    path("utente/<str:email>/", UtenteDetailAPIView.as_view(), name="utente-detail"),
 ]
